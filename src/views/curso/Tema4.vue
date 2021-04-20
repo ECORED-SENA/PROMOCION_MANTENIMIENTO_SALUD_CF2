ef549574f11795f5ff27c2f4dd2bf5918101ace7
elrilacho <80546125+elrilacho@users.noreply.github.com>
2021-04-20 11:02:34 -0500

    V1.0
    Componente listo para revisión

--- src/views/curso/Tema4.vue
+++ src/views/curso/Tema4.vue
@@ -7,18 +7,162 @@
         .h3 4
       .h3 Intervenciones individuales - Educación para la salud
     .row
-      .col-lg-4
+      .col-lg-6
         .bloque-texto-c.p-4
           .h2.mb-2 La educación no es la palanca para transformar el mundo… pero podría serlo.
           span - Paulo Freire
+      .col-lg-6
+        figure
+          img(src='@/assets/curso/img21.svg', alt='Texto que describa la imagen' style='width:60%').m-auto
+    .cajon--rojo.p-4.my-5.p-5
+          p “Es el proceso pedagógico (dialógico e intencionado) de construcción de conocimiento y aprendizaje que, mediante el diálogo de saberes, pretende construir o fortalecer el potencial de las personas, familias, comunidades y organizaciones para promover el cuidado de la salud, gestionar el riesgo en salud y transformar positivamente los entornos en los que se desenvuelven sus vidas” 
+            b (Resolución 3280 de 2018).  
+    p 
+      b Algunas consideraciones para entender la Educación para la Salud para la
+      br
+      b promoción y mantenimiento de la salud. 
+    ul.lista-ul--color.my-5
+      li.lista-separada.mb-5  
+        i.fas.fa-check.mx-2
+        p Implica la planeación y desarrollo de acciones educativas que incorporen un diseño pedagógico dirigido a generar la oportunidad para que lpersonas, familias y/o comunidades: mejoren conocimientos en relación a salud, desarrollen habilidades para el cuidado de la salud y reflexionacerca de acciones colectivas que aporten a la transformación de condiciones de vida relacionadas con la salu
+      li.lista-separada.my-5  
+        i.fas.fa-check.mx-2
+        p Aporta al desarrollo de la autorrealización, autogestión y autonomía individual y colectiva, como expresión de la garantía del derecho a la salud. 
+      li.lista-separada 
+        i.fas.fa-check.mx-2
+        p Está organizada desde la lógica de la salud (no desde la lógica de la enfermedad) y se refleja en el desarrollo de capacidades personales, familiares y comunitarias para comprender y transformar la (su) realidad y por tanto para convertirse en agentes sociales de cambio. 
+      li.lista-separada.my-5  
+        i.fas.fa-check.mx-2
+        p Parte de educar en derechos y está orientada hacia el desarrollo de los sujetos y sus capacidades de cuidado, que generen transformaciones en la salud y la calidad de vida, fortaleciendo la capacidad de agencia, reconociendo saberes que permiten hacer, comprender, reflexionar, sentir - expresar, innovar y ser y valorando la particularidad de esos saberes que construimos todos y todas.
+      li.lista-separada.mt-5 
+        i.fas.fa-check.mx-2
+        p Aunque se complementa con la información en salud, se distancia de esta en su alcance y objetivo. La información en salud apunta a orientar, advertir, anunciar o recomendar a sujetos y actores acerca de aspectos relacionados con la salud, para aportar a la construcción de elementos que propicien la toma de decisiones en salud. Para lograrlo, genera mecanismos o estrategias para disponer dicha información a unos públicos diversos (que incluye también los escenarios de atención en salud). La información en salud no entabla un proceso (una construcción que se da en el tiempo) de tipo reflexivo y dialógico (que conversa con los saberes del otro para recrearlos y construir posibilidades para la acción) que parte de reconocer saberes y capacidades en los sujetos; lo que le corresponde a la educación para la salud.
+    p La educación (y comunicación) para la salud se desarrolla tanto para las RIAS de riesgo como para la RPMS, el siguiente esquema nos permite comprender de qué manera se complementan estos escenarios:
+    figure.my-5
+      img(src='@/assets/curso/img22.svg', alt='Texto que describa la imagen' style='width:70%').m-auto
+    p.text-center 
+      b Fuente: Marco metodológico EpS (MinSalud, 2018**) p. 39
+    p.my-5 La educación para la salud, puede brindarse bajo la forma de educación individual y grupal (como parte de las atenciones y procedimientos con cargo a la UPC, es decir bajo la responsabilidad de la EPS o asegurador; así como educación colectiva es decir intervención colectiva (bajo responsabilidad de la entidad territorial de salud y la ESE como operador). 
+    .h4.titulo-rosa.text-center Educación individual - grupal - colectiva
+    .h4.text-center ¿A Quienes?
+    .tarjetas_container.my-5
+      .tarjetas_container--gris.py-5.px-4.m-3
+        img(src='@/assets/curso/img23.svg', alt='Texto que describa la imagen' style='width:92px')
+        .h4.my-3 Individual
+        p Dirigida a un sujeto en un momento de curso de vida o a una familia.
+          br
+          br
+          |A cargo de EAPB - Asegurador con la red de prestaciones de servicios
+      .tarjetas_container--gris.py-5.px-4.m-3
+        img(src='@/assets/curso/img24.svg', alt='Texto que describa la imagen' style='width:92px')
+        .h4.my-3 Grupal
+        p Dirigida a personas que comparten determinado momento de curso de vida o a grupos de personas o familias que comparten alguna condición o situación.
+      .tarjetas_container--gris.py-5.px-4.m-3
+        img(src='@/assets/curso/img25.svg', alt='Texto que describa la imagen' style='width:92px')
+        .h4.my-3 Colectiva
+        p Dirigido a colectivos que comparten una condición o situación, personas que viven o se desarrollan en determinado entorno (algunos comparten además el mismo momento de curso de vida.)
+          br
+          br
+          b A Cargo de la DTS con la ESE en el marco del Plan de la Salud de intervenciones Colectivas
+    p.mb-5 Las sesiones educativas deben ser implementadas por uno o varios profesionales, técnicos y tecnólogos de las ciencias de la salud, salud ambiental y de las ciencias sociales y humanas, con el apoyo de auxiliares del equipo de salud con las competencias para desarrollar metodológica, pedagógica y técnicamente el proceso (ideal el apoyo de profesional en pedagogía, psicopedagogía o psicología educativa). Para el caso de comunidades indígenas, podrán ser desarrolladas por el personal de salud propio de dichos pueblos.
+    .h4.titulo-rosa.text-center Educación individual - grupal
+    .h4.text-center ¿Como?
+    .tarjetas_container.my-5
+      .tarjetas_container--gris.py-5.px-4.m-3
+        img(src='@/assets/curso/img23.svg', alt='Texto que describa la imagen' style='width:92px')
+        .h4.my-3 Individual
+        p Considerando condiciones propias estableciendo una tutoría con plan de actividades o plan de acompañamiento e incorporando técnicas de consejería.
+      .tarjetas_container--gris.py-5.px-4.m-3
+        img(src='@/assets/curso/img24.svg', alt='Texto que describa la imagen' style='width:92px')
+        .h4.my-3 Grupal
+        p Considerando la caracterización poblacional y las características del momento de curso de vida y a través de la definición de ciclos de sesiones incorporando talleres, estudio de casos, seminarios, entre otros
+    p.mb-5 Las capacidades y los contenidos de los ciclos de educación para la salud pueden ser ajustados de acuerdo con i) la identificación de capacidades a ser fortalecidas con las personas o familias, ii) las necesidades de salud identificadas, y iii) las inquietudes de los grupos con los que se trabajará y iv) las orientaciones pedagógicas, metodológicas y didácticas contenidas en el documento de marco conceptual y metodológico de educación (y comunicación) para la salud en el marco de las Rutas Integrales de Atención en Salud – RIAS.
+    .h4.text-center Sesiones
+    .tarjetas_container.my-5
+      .tarjetas_container--gris.py-5.px-4.m-3
+        img(src='@/assets/curso/img23.svg', alt='Texto que describa la imagen' style='width:92px')
+        .h4.my-3 Individual
+        p De acuerdo al plan de acompañamiento establecido.
+          br
+          br
+          |Sesiones de 20 a 30 minutos (establecer formas de seguimiento y acompañamiento complementario virtual o telefónico)
+          br
+          br
+          |Sesiones se desarrollan con una persona (dos si requiere acompañante) en caso de la familia implica mínimo 2 miembros en la sesión.
+      .tarjetas_container--gris.py-5.px-4.m-3
+        img(src='@/assets/curso/img24.svg', alt='Texto que describa la imagen' style='width:92px')
+        .h4.my-3 Grupal
+        p2 Ciclos por momento de curso de vida cada ciclo con mínimo 3 máximo 6 sesiones. 
+        br
+        br
+        |Debe establecerse por parte de la EAPB- asegurador, una oferta permanente de ciclos educativos para garantizar cobertura de sus afiliados en sus diferentes momentos de curso de vida.
+        br
+        br
+        |Sesiones con grupos de máximo 70 personas; entre 6 a 15 familias.
+    p.mb-5 Al ser un proceso se requiere planear y desarrollar ciclos apuntando a los resultados en salud propuestos según el momento de curso de vida. Para el caso de la educación grupal, los ciclos educativos deben tener de 3 a 6 sesiones desarrolladas en un máximo de 3 meses de tal manera que se cumpla con los objetivos del proceso, para cada momento de curso de vida se deben desarrollar 2 ciclos educativos. 
+    .cajon--gris.p-4.my-5.p-5
+          p En el marco de la RPMS, se han definido objetivos específicos por momento de curso de vida, valorando lo que cada momento implica y con base en la premisa del desarrollo de capacidades de cuidado, de agencia y para ejercer sus derechos en especial el derecho a la salud, que pueden resumirse bajo el siguiente esquema:
+    .tabla-a.mb-5 
+      table
+        thead
+          tr
+            th Momento de curso de vida
+            th Objetivos de la educación para la salud
+            th Resultados en salud - Educación en salud por momento de curso de vida
+        tbody
+          tr
+            td
+              p.titulo-rosa.text-center 
+                b Primera infancia e infancia
+              figure.mt-3
+                img(src='@/assets/curso/img26.svg', alt='Texto que describa la imagen' style='width:150px').m-auto
+            td.p-5 Participan padres y cuidadores y gradualmente se incorporan de manera más activa los niños y niñas. Capacidades para la crianza, el cuidado de la salud y la protección que involucre así mismo a familias y entornos, orientados al desarrollo humano integral de niños(as).
+            td.p-5 Los resultados en salud están centrados en las capacidades de cuidado relacionadas con el cuidado de la salud, la crianza promotora de la salud y el desarrollo, la alimentación y nutrición; así como con la identificación de signos de alarma.
+              br
+              |Para la infancia se incorpora el afrontamiento de sucesos vitales y el fortalecimiento de capacidades socioemocionales como resultados en salud.
+          tr
+            td
+              p.titulo-rosa.text-center 
+                b Adolescencia
+              figure.mt-3
+                img(src='@/assets/curso/img27.svg', alt='Texto que describa la imagen' style='width:150px').m-auto
+            td.p-5 Reforzar potencialidades en lo relacionado con su capacidad de agencia, autonomía y construcción de identidad. Acompañar en los procesos de cambio propios del momento fisiológicos y psicológicos, pero también en relación al proyecto de vida, la sana convivencia, la sexualidad y la reproducción.
+            td.p-5 Los resultados en salud están centrados en las capacidades de  cuidado de la salud, la alimentación y nutrición, el ejercicio de los derechos sexuales y los derechos reproductivos, las habilidades socioemocionales para la promoción de la salud mental, el establecimiento de relaciones sanas y constructivas y el afrontamiento de sucesos vitales.
+          tr
+            td
+              p.titulo-rosa.text-center 
+                b Juventud
+              figure.mt-3
+                img(src='@/assets/curso/img28.svg', alt='Texto que describa la imagen' style='width:150px').m-auto
+            td.p-5 Capacidades para transformar saberes y prácticas en pro de una vida saludable, relacionadas con el ejercicio de la sexualidad y la reproducción libre e informada, la sana convivencia, la educación para el trabajo y el desarrollo humano en los diferentes escenarios durante el curso de vida, la capacidad política y la autonomía para agenciarse y relacionarse con otros/as
+            <td rowspan="3"> Los resultados en salud están centrados en las capacidades de cuidado relacionadas con el cuidado de la salud, la alimentación y nutrición, el ejercicio de los derechos sexuales y los derechos reproductivos, las habilidades socioemocionales para la promoción de la salud mental, el establecimiento de relaciones sanas y constructivas y el afrontamiento de sucesos vitales.</td>
+          tr
+            td
+              p.titulo-rosa.text-center 
+                b Adultez 
+              figure.mt-3
+                img(src='@/assets/curso/img29.svg', alt='Texto que describa la imagen' style='width:150px').m-auto
+            td.p-5 Capacidades para la promoción y mantenimiento de su salud en reconocimiento de su dimensión laboral, familiar y personal; así como las capacidades para ejercer sus derechos y el derecho a la salud.
+          tr
+            td
+              p.titulo-rosa.text-center 
+                b Vejez
+              figure.mt-3
+                img(src='@/assets/curso/img30.svg', alt='Texto que describa la imagen' style='width:150px').m-auto
+            td.p-5 Capacidades para la promoción y mantenimiento de su salud; así como las capacidades para ejercer sus derechos y el derecho a la salud. Aportar para mantener conservadas, el mayor tiempo posible su autonomía y las habilidades intelectuales (atención, memoria, funciones ejecutivas, aprendizaje, razonamiento, percepción, cálculo, praxias).
 
-      .col-lg-8
+
+          
+
+
+
+      
 </template>
 
 <script>
 import BannerInterno from '../../components/plantilla/BannerInterno'
 export default {
-  name: 'Tema3',
+  name: 'Tema4',
   components: {
     BannerInterno,
   },
